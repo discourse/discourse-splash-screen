62fe2a3418b5c4721fbb4d475d4c38c231a60b73
Penar Musaraj <pmusaraj@gmail.com>
2024-02-07 14:59:07 -0500

    UX: Updated to use a single image for all slides  (#7)
    - uses one image for all slides
    - switches to translations for slide descriptions
    - adds a setting for the number of slides to show (max 5)
    - removes json schema setting

--- spec/system/splash_screen_spec.rb
+++ spec/system/splash_screen_spec.rb
@@ -5,12 +5,14 @@ require_relative "page_objects/components/splash_screen"
 RSpec.describe "Splash screen spec", system: true do
   let!(:theme_component) { upload_theme_component }
   let(:splash_screen) { PageObjects::Components::SplashScreen.new }
-  let!(:settings_data) { '[{"title":"Welcome to Our App","description":"Explore the amazing features and functionalities of our app.","background_image_url":"https://example.com/background1.jpg"},{"title":"Discover Exciting Possibilities","description":"Dive into a world of innovation and possibilities with our app.","background_image_url":"https://example.com/background2.jpg"},{"title":"Connect with Others","description":"Build meaningful connections and share experiences with our community.","background_image_url":"https://example.com/background3.jpg"},{"title":"Unleash Your Creativity","description":"Express yourself and unleash your creativity using our powerful tools.","background_image_url":"https://example.com/background4.jpg"},{"title":"Ready to Get Started?","description":"Join us now and experience a new level of convenience and excitement.","background_image_url":"https://example.com/background5.jpg"}]' }
-  let!(:settings_array) { JSON.parse(settings_data) }
+
   fab!(:user)
 
   before do
-    theme_component.update_setting(:slide_data, settings_data)
+    theme_component.update_translation("slides.slide1", "Slide 1 description")
+    theme_component.update_translation("slides.slide2", "Slide 2 description")
+    theme_component.update_translation("slides.slide3", "Slide 3 description")
+    theme_component.update_translation("slides.heading", "Community")
     theme_component.save!
   end
 
@@ -40,17 +42,24 @@ RSpec.describe "Splash screen spec", system: true do
 
     it "should show the correct title and description" do
       visit("/")
-      expect(splash_screen).to have_heading(settings_array[0]["title"])
-      expect(splash_screen).to have_description(settings_array[0]["description"])
+      heading = theme_component.translations.detect {|e| e.key == "slides.heading" }
+      slide1 = theme_component.translations.detect {|e| e.key == "slides.slide1" }
+
+      expect(splash_screen).to have_heading(heading.value)
+      expect(splash_screen).to have_description(slide1.value)
     end
 
     it "should change to the next slide when clicking the next button" do
       visit("/")
-      expect(splash_screen).to have_heading(settings_array[0]["title"])
-      expect(splash_screen).to have_description(settings_array[0]["description"])
+
+      heading = theme_component.translations.detect {|e| e.key == "slides.heading" }
+      slide1 = theme_component.translations.detect {|e| e.key == "slides.slide1" }
+      slide2 = theme_component.translations.detect {|e| e.key == "slides.slide2" }
+
+      expect(splash_screen).to have_heading(heading.value)
+      expect(splash_screen).to have_description(slide1.value)
       splash_screen.click_next_button
-      expect(splash_screen).to have_heading(settings_array[1]["title"])
-      expect(splash_screen).to have_description(settings_array[1]["description"])
+      expect(splash_screen).to have_description(slide2.value)
     end
 
     it "should skip to the login page when clicking the skip button" do
@@ -62,14 +71,18 @@ RSpec.describe "Splash screen spec", system: true do
 
     it "should go to the page when clicking on the indicator dot" do
       visit("/")
+      heading = theme_component.translations.detect {|e| e.key == "slides.heading" }
+      slide3 = theme_component.translations.detect {|e| e.key == "slides.slide3" }
+
       splash_screen.click_indicator(3)
-      expect(splash_screen).to have_heading(settings_array[2]["title"])
-      expect(splash_screen).to have_description(settings_array[2]["description"])
+      expect(splash_screen).to have_heading(heading.value)
+      expect(splash_screen).to have_description(slide3.value)
     end
 
     it "should go to the login page after clicking through all the slides" do
       visit("/")
-      5.times { splash_screen.click_next_button }
+      # Defaults to 3 slides
+      3.times { splash_screen.click_next_button }
       expect(page).to have_css(".login-modal")
       expect(splash_screen).to have_no_splash_screen
     end
@@ -77,11 +90,10 @@ RSpec.describe "Splash screen spec", system: true do
     context "when the user has already seen the splash screen" do
       before { visit("/") }
 
-      it "should default to the last page of the splash screen" do
+      it "should not show the splash screen" do
         visit("/")
 
-        expect(splash_screen).to have_heading(settings_array[settings_array.length - 1]["title"])
-        expect(splash_screen).to have_description(settings_array[settings_array.length - 1]["description"])
+        expect(splash_screen).to have_no_splash_screen
       end
     end
   end
